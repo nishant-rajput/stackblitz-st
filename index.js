919f87ff4dc19458736c1053ddde3439c510530c
nishant-rajput <udnr26@gmail.com>
2024-11-20 09:34:18 +0530

    new changes

--- index.js
+++ index.js
@@ -1,130 +1,70 @@
-const express = require('express');
+let express = require("express");
+let cors = require("cors");
 const { resolve } = require('path');
 
-const app = express();
+let app = express();
 const port = 3000;
+app.use(cors());
+
+let taxRate = 5; 
+let discountPercentage = 10; 
+let loyaltyRate = 2;
 
 app.use(express.static('static'));
 
-app.get('/shout', (req, res) => {
-  let name = req.query.name;
-  let upper = name.toUpperCase();
-  res.send(upper);
+app.get('/calculate-tax', (req, res) => {
+  let cartTotal = parseFloat(req.query.cartTotal);
+  let taxAmount = cartTotal * (taxRate / 100);
+  res.send(taxAmount.toString());
 });
 
-app.get('/fullName', (req, res) => {
-  let first = req.query.firstName;
-  let last = req.query.lastName;
-  let fullName = first + ' ' + last;
-  res.send(fullName);
-});
+app.get('/estimate-delivery', (req, res) => {
+  let shippingMethod = req.query.shippingMethod;
+  let distance = parseFloat(req.query.distance);
+  let deliveryDays;
 
-app.get('/date', (req, res) => {
-  let mon = req.query.month;
-  let yr = req.query.year;
-  let dateFull = mon + ', ' + yr;
-  res.send(dateFull);
-});
-
-app.get('/greeting', (req, res) => {
-  let nm = req.query.name;
-
-  let wish = 'Namaste, ' + nm + '!';
-  res.send(wish);
-});
-app.get('/email', (req, res) => {
-  let usr = req.query.username;
-  let dom = req.query.domain;
-
-  let ema = usr + '@' + dom;
-  res.send(ema);
-});
-
-app.get('/address', (req, res) => {
-  let stree = req.query.street;
-  let cit = req.query.city;
-  let sta = req.query.state;
-
-  let wish = stree + ', ' + cit + ', ' + sta;
-  res.send(wish);
-});
-
-app.get('/total-distance', (req, res) => {
-  let d1 = parseFloat(req.query.distance1);
-  let d2 = parseFloat(req.query.distance2);
-
-  let totalDistance = d1 + d2;
-  res.send(totalDistance.toString());
-});
-
-app.get('/total-time', (req, res) => {
-  let t1 = parseFloat(req.query.time1);
-  let t2 = parseFloat(req.query.time2);
-  let t3 = parseFloat(req.query.time3);
-
-  let totalTime = t1 + t2 + t3;
-  res.send(totalTime.toString());
-});
-
-app.get('/average-speed', (req, res) => {
-  let td = parseFloat(req.query.totalDistance);
-  let tt = parseFloat(req.query.totalTime);
-
-  let speedTotal = td / tt;
-  res.send(speedTotal.toString());
-});
-
-app.get('/eta', (req, res) => {
-  let dist = parseFloat(req.query.distance);
-  let spee = parseFloat(req.query.speed);
-
-  let time = dist / spee;
-  res.send(time.toString());
-});
-
-app.get('/total-calories', (req, res) => {
-  let d1 = parseFloat(req.query.duration1);
-  let d2 = parseFloat(req.query.duration2);
-  let cpm = parseFloat(req.query.caloriesPerMinute);
-
-  let totalCalories = (d1 + d2) * cpm;
-  res.send(totalCalories.toString());
-});
-
-app.get('/interest-earned', (req, res) => {
-  let pr = parseFloat(req.query.principal);
-  let rt = parseFloat(req.query.rate);
-  let tm = parseFloat(req.query.time);
-
-  let interest = (pr * rt * tm) / 100;
-  res.send(interest.toString());
-});
-
-app.get('/check-number', (req, res) => {
-  let num = parseFloat(req.query.number);
-  if (num >= 0) {
-    res.send('Number is positive');
+  if (shippingMethod.toLowerCase() === 'standard') {
+    deliveryDays = Math.ceil(distance / 50);
+  } else if (shippingMethod.toLowerCase() === 'express') {
+    deliveryDays = Math.ceil(distance / 100);
   } else {
-    res.send('Number is negative');
+    return res.status(400).send('Invalid shipping method');
   }
+
+  res.send(deliveryDays.toString());
 });
 
-app.get('/check-even-odd', (req, res) => {
-  let num = parseFloat(req.query.number);
-  if (num % 2 == 0) {
-    res.send('Number is even');
-  } else {
-    res.send('Number is odd');
-  }
+app.get('/shipping-cost', (req, res) => {
+  let weight = parseFloat(req.query.weight);
+  let distance = parseFloat(req.query.distance);
+  let shippingCost = weight * distance * 0.1;
+  res.send(shippingCost.toString());
 });
 
-app.get('/check-login', (req, res) => {
-  let num = req.query.isLoggedIn === 'true';
-  if (num == true) {
-    res.send('User is logged in');
-  } else {
-    res.send('User is not logged in');
+app.get('/loyalty-points', (req, res) => {
+  let purchaseAmount = parseFloat(req.query.purchaseAmount);
+  let loyaltyPoints = purchaseAmount * loyaltyRate;
+  res.send(loyaltyPoints.toString());
+});
+
+app.get('/cart-total', (req, res) => {
+  let newItemPrice = parseFloat(req.query.newItemPrice)
+  let cartTotal = parseFloat(req.query.cartTotal)
+  let totalPrice = newItemPrice + cartTotal
+  res.send(totalPrice.toString())  // Remove the 's' at the end of this line
+});
+
+app.get('/membership-discount', (req, res) => {
+  let newItemPrice = parseFloat(req.query.cartTotal)
+  let isMember = req.query.isMember === 'true'  // Changed to string comparison
+  let totalPrice;
+  if(isMember){
+    totalPrice = newItemPrice - newItemPrice*(discountPercentage/100)
   }
+  else{
+    totalPrice = newItemPrice
+  }
+  res.send(totalPrice.toString())
 });
 
 app.listen(port, () => {
